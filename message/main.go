9896446b7051c731c8e146fcff7419bde914b10e
Luke Rohenaz <rohenaz@gmail.com>
2024-02-05 08:42:14 -0500

    decrypt wip

--- message/main.go
+++ message/main.go
@@ -1,8 +1,10 @@
 package message
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 
 	"github.com/bitcoin-sv/go-sdk/aesgcm"
 	"github.com/bitcoin-sv/go-sdk/ec"
@@ -119,3 +121,63 @@ func Encrypt(message []byte, sender *ec.PrivateKey, recipient *ec.PublicKey) (*M
 // 	fmt.Printf("Message Point: (%s, %s)\n", point.X.String(), point.Y.String())
 // 	fmt.Printf("cId: %s\n", cId)
 // }
+
+// /**
+//   - Decrypts a message from one party to another using the BRC-78 message encryption protocol.
+//   - @param message The message to decrypt
+//   - @param sender The private key of the recipient
+//     *
+//   - @returns The decrypted message
+//     */
+func Decrypt(message []byte, recipient *ec.PrivateKey) ([]byte, error) {
+	messageVersion := message[:4]
+	if string(messageVersion) != VERSION {
+		return nil, fmt.Errorf("Message version mismatch: Expected %s, received %s", VERSION, messageVersion)
+	}
+	reader := bytes.NewReader(message)
+	reader.Seek(4, 0)
+	senderPublicKey := make([]byte, 33)
+	_, err := reader.Read(senderPublicKey)
+	if err != nil {
+		return nil, err
+	}
+	sender, err := ec.ParsePubKey(senderPublicKey, ec.S256())
+	if err != nil {
+		return nil, err
+	}
+
+	expectedRecipientDER := make([]byte, 33)
+	_, err = reader.Read(expectedRecipientDER)
+	if err != nil {
+		return nil, err
+	}
+	actualRecipientDER := recipient.PubKey().SerialiseCompressed()
+	if !bytes.Equal(expectedRecipientDER, actualRecipientDER) {
+		return nil, fmt.Errorf("the encrypted message expects a recipient public key of %x, but the provided key is %x", expectedRecipientDER, actualRecipientDER)
+	}
+	keyID := make([]byte, 32)
+	_, err = reader.Read(keyID)
+	if err != nil {
+		return nil, err
+	}
+	encrypted := make([]byte, reader.Len())
+	_, err = reader.Read(encrypted)
+	if err != nil {
+		return nil, err
+	}
+	invoiceNumber := "2-message encryption-" + hex.EncodeToString(keyID)
+	signingPriv, err := sender.DeriveChild(recipient, invoiceNumber)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: This is weird from ts library - its called recipientPub but its a private key
+	recipientPub, err := recipient.DeriveChild(recipient.PubKey(), invoiceNumber)
+	if err != nil {
+		return nil, err
+	}
+	sharedSecret, err := signingPriv.DeriveSharedSecret(recipientPub)
+	if err != nil {
+		return nil, err
+	}
+	return aesgcm.DecryptGCM(encrypted, sharedSecret.SerialiseCompressed(), keyID, nil, nil)
+}
