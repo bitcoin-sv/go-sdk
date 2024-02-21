64bbef555305793082e6a64d167bb905d327c92b
David Case <david@fyxgaming.com>
2024-02-21 16:23:57 -0500

    feat: script interpreter

--- script/script.go
+++ script/script.go
@@ -2,16 +2,11 @@ package script
 
 import (
 	"bytes"
-	"crypto/rand"
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"math/bits"
 	"strings"
-
-	"github.com/bitcoin-sv/go-sdk/bip32"
-	"github.com/bitcoin-sv/go-sdk/crypto"
-	"github.com/bitcoin-sv/go-sdk/ec"
 )
 
 // ScriptKey types.
@@ -59,103 +54,6 @@ func NewFromASM(str string) (*Script, error) {
 	}
 
 	return &s, nil
-}
-
-// NewP2PKHFromPubKeyEC takes a public key hex string (in
-// compressed format) and creates a P2PKH script from it.
-func NewP2PKHFromPubKeyEC(pubKey *ec.PublicKey) (*Script, error) {
-	return NewP2PKHFromPubKeyBytes(pubKey.SerialiseCompressed())
-}
-
-// NewP2PKHFromPubKeyStr takes a public key hex string (in
-// compressed format) and creates a P2PKH script from it.
-func NewP2PKHFromPubKeyStr(pubKey string) (*Script, error) {
-	pubKeyBytes, err := hex.DecodeString(pubKey)
-	if err != nil {
-		return nil, err
-	}
-	return NewP2PKHFromPubKeyBytes(pubKeyBytes)
-}
-
-// NewP2PKHFromPubKeyBytes takes public key bytes (in
-// compressed format) and creates a P2PKH script from it.
-func NewP2PKHFromPubKeyBytes(pubKeyBytes []byte) (*Script, error) {
-	if len(pubKeyBytes) != 33 {
-		return nil, ErrInvalidPKLen
-	}
-	return NewP2PKHFromPubKeyHash(crypto.Hash160(pubKeyBytes))
-}
-
-// NewP2PKHFromPubKeyHash takes a public key hex string (in
-// compressed format) and creates a P2PKH script from it.
-func NewP2PKHFromPubKeyHash(pubKeyHash []byte) (*Script, error) {
-	b := []byte{
-		OpDUP,
-		OpHASH160,
-		OpDATA20,
-	}
-	b = append(b, pubKeyHash...)
-	b = append(b, OpEQUALVERIFY)
-	b = append(b, OpCHECKSIG)
-
-	s := Script(b)
-	return &s, nil
-}
-
-// NewP2PKHFromPubKeyHashStr takes a public key hex string (in
-// compressed format) and creates a P2PKH script from it.
-func NewP2PKHFromPubKeyHashStr(pubKeyHash string) (*Script, error) {
-	hash, err := hex.DecodeString(pubKeyHash)
-	if err != nil {
-		return nil, err
-	}
-
-	return NewP2PKHFromPubKeyHash(hash)
-}
-
-// NewP2PKHFromAddress takes an address
-// and creates a P2PKH script from it.
-func NewP2PKHFromAddress(addr string) (*Script, error) {
-	a, err := NewAddressFromString(addr)
-	if err != nil {
-		return nil, err
-	}
-
-	var publicKeyHashBytes []byte
-	if publicKeyHashBytes, err = hex.DecodeString(a.PublicKeyHash); err != nil {
-		return nil, err
-	}
-
-	s := new(Script)
-	_ = s.AppendOpcodes(OpDUP, OpHASH160)
-	if err = s.AppendPushData(publicKeyHashBytes); err != nil {
-		return nil, err
-	}
-	_ = s.AppendOpcodes(OpEQUALVERIFY, OpCHECKSIG)
-
-	return s, nil
-}
-
-// NewP2PKHFromBip32ExtKey takes a *bip32.ExtendedKey and creates a P2PKH script from it,
-// using an internally random generated seed, returning the script and derivation path used.
-func NewP2PKHFromBip32ExtKey(privKey *bip32.ExtendedKey) (*Script, string, error) {
-	var b [8]byte
-	if _, err := rand.Read(b[:]); err != nil {
-		return nil, "", err
-	}
-
-	derivationPath := bip32.DerivePath(binary.LittleEndian.Uint64(b[:]))
-	pubKey, err := privKey.DerivePublicKeyFromPath(derivationPath)
-	if err != nil {
-		return nil, "", err
-	}
-
-	lockingScript, err := NewP2PKHFromPubKeyBytes(pubKey)
-	if err != nil {
-		return nil, "", err
-	}
-
-	return lockingScript, derivationPath, nil
 }
 
 // AppendPushData takes data bytes and appends them to the script
@@ -521,7 +419,7 @@ func (s *Script) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`"%s"`, s.String())), nil
 }
 
-// UnmarshalJSON covert from json into *bscript.Script.
+// UnmarshalJSON covert from json into *script.Script.
 func (s *Script) UnmarshalJSON(bb []byte) error {
 	ss, err := NewFromHexString(string(bytes.Trim(bb, `"`)))
 	if err != nil {
