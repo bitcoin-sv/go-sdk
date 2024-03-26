a1183ff49a045138e4b27f11908c769d03f941c9
Jad Wahab <15110087+jadwahab@users.noreply.github.com>
2024-03-26 23:09:43 +0300

    merklepath to hex working

--- transaction/merklePath.go
+++ transaction/merklePath.go
@@ -1,11 +1,11 @@
 package transaction
 
 import (
-	"bytes"
 	"encoding/hex"
-	"io"
+	"fmt"
 	"sort"
 
+	"github.com/bitcoin-sv/go-sdk/transaction/chaintracker"
 	"github.com/pkg/errors"
 )
 
@@ -39,102 +39,102 @@ func NewMerklePathFromHex(hexData string) (*MerklePath, error) {
 }
 
 // NewMerklePathFromBinary creates a new MerklePath with the given binary
-func NewMerklePathFromBinary(bin []byte) (*MerklePath, error) {
-	mp := &MerklePath{}
-	_, err := mp.ReadFrom(bytes.NewReader(bin))
-	if err != nil {
-		return nil, err
+func NewMerklePathFromBinary(bytes []byte) (*MerklePath, error) {
+	if len(bytes) < 37 {
+		return nil, errors.New("BUMP bytes do not contain enough data to be valid")
 	}
-	return mp, nil
+	bump := &MerklePath{}
+
+	// first bytes are the block height.
+	var skip int
+	index, size := NewVarIntFromBytes(bytes[skip:])
+	skip += size
+	bump.BlockHeight = uint64(index)
+
+	// Next byte is the tree height.
+	treeHeight := uint(bytes[skip])
+	skip++
+
+	// We expect tree height levels.
+	bump.Path = make([][]PathElement, treeHeight)
+
+	for lv := uint(0); lv < treeHeight; lv++ {
+		// For each level we parse a bunch of nLeaves.
+		n, size := NewVarIntFromBytes(bytes[skip:])
+		skip += size
+		nLeavesAtThisHeight := uint64(n)
+		if nLeavesAtThisHeight == 0 {
+			return nil, errors.New("There are no leaves at height: " + fmt.Sprint(lv) + " which makes this invalid")
+		}
+		bump.Path[lv] = make([]PathElement, nLeavesAtThisHeight)
+		for lf := uint64(0); lf < nLeavesAtThisHeight; lf++ {
+			// For each leaf we parse the offset, hash, txid and duplicate.
+			offset, size := NewVarIntFromBytes(bytes[skip:])
+			skip += size
+			var l PathElement
+			o := uint64(offset)
+			l.Offset = o
+			flags := bytes[skip]
+			skip++
+			dup := flags&1 > 0
+			txid := flags&2 > 0
+			if dup {
+				l.Duplicate = dup
+			} else {
+				if len(bytes) < skip+32 {
+					return nil, errors.New("BUMP bytes do not contain enough data to be valid")
+				}
+				h := bytes[skip : skip+32]
+				l.Hash = h
+				skip += 32
+			}
+			if txid {
+				l.Txid = txid
+			}
+			bump.Path[lv][lf] = l
+		}
+	}
+
+	// Sort each of the levels by the offset for consistency.
+	for _, level := range bump.Path {
+		sort.Slice(level, func(i, j int) bool {
+			return level[i].Offset < level[j].Offset
+		})
+	}
+
+	return bump, nil
 }
 
-// NewMerklePathFromBinary creates a new MerklePath with the given binary data
-func (mp *MerklePath) ReadFrom(r io.Reader) (int64, error) {
-	var bytesRead int64
-
-	var blockHeight VarInt
-	n64, err := blockHeight.ReadFrom(r)
-	bytesRead += n64
-	if err != nil {
-		return bytesRead, err
-	}
-
-	treeHeight := make([]byte, 1)
-	n, err := io.ReadFull(r, treeHeight)
-	bytesRead += int64(n)
-	if err != nil {
-		return bytesRead, err
-	}
-	th := uint8(treeHeight[0])
-
-	path := make([][]PathElement, th)
-
-	for level := 0; level < int(th); level++ {
-		var nLeavesAtThisHeight VarInt
-		n64, err := nLeavesAtThisHeight.ReadFrom(r)
-		bytesRead += n64
-		if err != nil {
-			return bytesRead, err
+// Bytes encodes a BUMP as a slice of bytes. BUMP Binary Format according to BRC-74 https://brc.dev/74
+func (mp *MerklePath) Bytes() []byte {
+	bytes := []byte{}
+	bytes = append(bytes, VarInt(mp.BlockHeight).Bytes()...)
+	treeHeight := len(mp.Path)
+	bytes = append(bytes, byte(treeHeight))
+	for level := 0; level < treeHeight; level++ {
+		nLeaves := len(mp.Path[level])
+		bytes = append(bytes, VarInt(nLeaves).Bytes()...)
+		for _, leaf := range mp.Path[level] {
+			bytes = append(bytes, VarInt(leaf.Offset).Bytes()...)
+			flags := byte(0)
+			if leaf.Duplicate {
+				flags |= 1
+			}
+			if leaf.Txid {
+				flags |= 2
+			}
+			bytes = append(bytes, flags)
+			if (flags & 1) == 0 {
+				bytes = append(bytes, ReverseBytes(leaf.Hash)...)
+			}
 		}
-
-		leaves := make([]PathElement, 0)
-
-		for nLeavesAtThisHeight > 0 {
-			var offset VarInt
-			n64, err := offset.ReadFrom(r)
-			bytesRead += n64
-			if err != nil {
-				return bytesRead, err
-			}
-
-			leaf := PathElement{
-				Offset: uint64(offset),
-			}
-			flags := make([]byte, 1)
-			n, err := io.ReadFull(r, flags)
-			bytesRead += int64(n)
-			if err != nil {
-				return bytesRead, err
-			}
-			f := uint8(flags[0])
-
-			if f&1 > 0 {
-				leaf.Duplicate = true
-			} else {
-				if f&2 > 0 {
-					leaf.Txid = true
-				}
-				hash := make([]byte, 32)
-				n, err := io.ReadFull(r, hash)
-				bytesRead += int64(n)
-				if err != nil {
-					return bytesRead, err
-				}
-				leaf.Hash = hash
-			}
-			leaves = append(leaves, leaf)
-			nLeavesAtThisHeight--
-		}
-		sort.Slice(leaves, func(i, j int) bool {
-			return leaves[i].Offset < leaves[j].Offset
-		})
-		path[level] = leaves
 	}
-
-	mp = &MerklePath{
-		BlockHeight: uint64(blockHeight),
-		Path:        path,
-	}
-
-	return 0, nil
+	return bytes
 }
 
 // ToHex converts the MerklePath to a hexadecimal string representation
 func (mp *MerklePath) ToHex() string {
-	// This function should implement the conversion of the MerklePath to its binary representation first,
-	// then convert that binary representation to a hexadecimal string.
-	// Placeholder implementation below - you'll need to replace this with actual logic.
-	return "hexadecimal_representation_here"
+	return hex.EncodeToString(mp.Bytes())
 }
 
 // ComputeRoot computes the Merkle root from a given transaction ID
@@ -144,16 +144,16 @@ func (mp *MerklePath) ComputeRoot(txid string) (string, error) {
 	return "", errors.New("computeRoot not implemented")
 }
 
-// // Verify checks if a given transaction ID is part of the Merkle tree at the specified block height using a chain tracker
-// func (mp *MerklePath) Verify(txid string, chainTracker *ChainTracker) (bool, error) {
-// 	// Placeholder for chain tracker interaction. You need to implement the verification logic here, possibly interacting with a chain tracker.
-// 	// This involves computing the Merkle root and verifying it against the chain tracker's data.
-// 	return false, errors.New("verify not implemented")
-// }
+// Verify checks if a given transaction ID is part of the Merkle tree at the specified block height using a chain tracker
+func (mp *MerklePath) Verify(txid string, chainTracker chaintracker.ChainTracker) (bool, error) {
+	// Placeholder for chain tracker interaction. You need to implement the verification logic here, possibly interacting with a chain tracker.
+	// This involves computing the Merkle root and verifying it against the chain tracker's data.
+	return false, errors.New("verify not implemented")
+}
 
-// // Combine combines this MerklePath with another to create a compound proof
-// func (mp *MerklePath) Combine(other *MerklePath) error {
-// 	// Placeholder implementation. Combining two Merkle paths involves ensuring they can be combined
-// 	// and then performing the combination logic.
-// 	return errors.New("combine not implemented")
-// }
+// Combine combines this MerklePath with another to create a compound proof
+func (mp *MerklePath) Combine(other *MerklePath) error {
+	// Placeholder implementation. Combining two Merkle paths involves ensuring they can be combined
+	// and then performing the combination logic.
+	return errors.New("combine not implemented")
+}
--- transaction/merklepath_test.go
+++ transaction/merklepath_test.go
@@ -139,7 +139,60 @@ func TestMerklePath_ComputeRoot(t *testing.T) {
 	})
 }
 
-// func TestMerklePath_Verify(t *testing.T) {
+// Define a struct that implements the ChainTracker interface.
+type MyChainTracker struct{}
+
+// Implement the IsValidRootForHeight method on MyChainTracker.
+func (mct MyChainTracker) IsValidRootForHeight(root []byte, height uint32) bool {
+	// Convert BRC74Root hex string to a byte slice for comparison
+	expectedRoot, _ := hex.DecodeString(BRC74Root)
+	// Assuming BRC74JSON.BlockHeight is of type uint64, and needs to be cast to uint32
+	return string(root) == string(expectedRoot) && uint32(height) == uint32(813706) // FIXME: change 813706 to get from json instead
+}
+
+func TestMerklePath_Verify(t *testing.T) {
+
+	BRC74JSON := MerklePath{
+		BlockHeight: 813706,
+		Path: [][]PathElement{
+			{
+				{Offset: 3048, Hash: hexToByte("304e737fdfcb017a1a322e78b067ecebb5e07b44f0a36ed1f01264d2014f7711")},
+				{Offset: 3049, Txid: true, Hash: hexToByte("d888711d588021e588984e8278a2decf927298173a06737066e43f3e75534e00")},
+				{Offset: 3050, Txid: true, Hash: hexToByte("98c9c5dd79a18f40837061d5e0395ffb52e700a2689e641d19f053fc9619445e")},
+				{Offset: 3051, Duplicate: true},
+			},
+			{
+				{Offset: 1524, Hash: hexToByte("811ae75c80fecd27efff5ef272c2adf7edb6e535447f27a4087d23724f397106")},
+				{Offset: 1525, Hash: hexToByte("82520a4501a06061dd2386fb92fa5e9ceaed14747acc00edf34a6cecabcc2b26")},
+			},
+			{{Offset: 763, Duplicate: true}},
+			{{Offset: 380, Hash: hexToByte("858e41febe934b4cbc1cb80a1dc8e254cb1e69acff8e4f91ecdd779bcaefb393")}},
+			{{Offset: 191, Duplicate: true}},
+			{{Offset: 94, Hash: hexToByte("f80263e813c644cd71bcc88126d0463df070e28f11023a00543c97b66e828158")}},
+			{{Offset: 46, Hash: hexToByte("f36f792fa2b42acfadfa043a946d4d7b6e5e1e2e0266f2cface575bbb82b7ae0")}},
+			{{Offset: 22, Hash: hexToByte("7d5051f0d4ceb7d2e27a49e448aedca2b3865283ceffe0b00b9c3017faca2081")}},
+			{{Offset: 10, Hash: hexToByte("43aeeb9b6a9e94a5a787fbf04380645e6fd955f8bf0630c24365f492ac592e50")}},
+			{{Offset: 4, Hash: hexToByte("45be5d16ac41430e3589a579ad780e5e42cf515381cc309b48d0f4648f9fcd1c")}},
+			{{Offset: 3, Duplicate: true}},
+			{{Offset: 0, Hash: hexToByte("d40cb31af3ef53dd910f5ce15e9a1c20875c009a22d25eab32c11c7ece6487af")}},
+		},
+	}
+
+	t.Run("verifies using a ChainTracker", func(t *testing.T) {
+		path := MerklePath{
+			BlockHeight: BRC74JSON.BlockHeight,
+			Path:        BRC74JSON.Path,
+		}
+		tracker := MyChainTracker{}
+		result, err := path.Verify(BRC74TXID1, tracker)
+		assert.NoError(t, err)
+		assert.True(t, result)
+	})
+
+}
+
+// func TestMerklePath_Combine(t *testing.T) {
+// 	t.Parallel()
 
 // 	BRC74JSON := MerklePath{
 // 		BlockHeight: 813706,
@@ -167,51 +220,32 @@ func TestMerklePath_ComputeRoot(t *testing.T) {
 // 		},
 // 	}
 
-// 	t.Run("verifies using a ChainTracker", func(t *testing.T) {
-// 		path := MerklePath{
-// 			BlockHeight: BRC74JSON.BlockHeight,
-// 			Path:        BRC74JSON.Path,
-// 		}
-// 		tracker := &ChainTracker{
-// 			IsValidRootForHeight: func(root string, height int) bool {
-// 				return root == BRC74Root && height == BRC74JSON.BlockHeight
-// 			},
-// 		}
-// 		result, err := path.Verify(BRC74TXID1, tracker)
-// 		assert.NoError(t, err)
-// 		assert.True(t, result)
-// 	})
-
-// }
-
-// func TestMerklePath_Combine(t *testing.T) {
-// 	t.Parallel()
-
-// 	t.Run("combines two paths", func(t *testing.T) {
-// 		path0A := make([]PathElement, len(BRC74JSON.Path[0]))
-// 		copy(path0A, BRC74JSON.Path[0])
-// 		path0B := make([]PathElement, len(BRC74JSON.Path[0]))
-// 		copy(path0B, BRC74JSON.Path[0])
-// 		pathRest := make([][]PathElement, len(BRC74JSON.Path)-1)
-// 		copy(pathRest, BRC74JSON.Path[1:])
-// 		path0A = append(path0A[:2], path0A[4:]...)
-// 		path0B = path0B[2:]
-// 		pathRest = pathRest[1:]
-// 		pathRest = append([][]PathElement{path0B}, pathRest...)
-// 		pathA := MerklePath{
-// 			BlockHeight: BRC74JSON.BlockHeight,
-// 			Path:        append([][]PathElement{path0A}, pathRest...),
-// 		}
-// 		pathB := MerklePath{
-// 			BlockHeight: BRC74JSON.BlockHeight,
-// 			Path:        append([][]PathElement{path0B}, pathRest...),
-// 		}
-// 		assert.Equal(t, BRC74Root, pathA.ComputeRoot(BRC74TXID2))
-// 		assert.Panics(t, func() { pathA.ComputeRoot(BRC74TXID3) })
-// 		assert.Panics(t, func() { pathB.ComputeRoot(BRC74TXID2) })
-// 		assert.Equal(t, BRC74Root, pathB.ComputeRoot(BRC74TXID3))
-// 		pathA.Combine(pathB)
-// 		assert.Equal(t, BRC74Root, pathA.ComputeRoot(BRC74TXID2))
-// 		assert.Equal(t, BRC74Root, pathA.ComputeRoot(BRC74TXID3))
-// 	})
-// }
+// // 	t.Run("combines two paths", func(t *testing.T) {
+// // 		path0A := make([]PathElement, len(BRC74JSON.Path[0]))
+// // 		copy(path0A, BRC74JSON.Path[0])
+// // 		path0B := make([]PathElement, len(BRC74JSON.Path[0]))
+// // 		copy(path0B, BRC74JSON.Path[0])
+// // 		pathRest := make([][]PathElement, len(BRC74JSON.Path)-1)
+// // 		copy(pathRest, BRC74JSON.Path[1:])
+// // 		path0A = append(path0A[:2], path0A[4:]...)
+// // 		path0B = path0B[2:]
+// // 		pathRest = pathRest[1:]
+// // 		pathRest = append([][]PathElement{path0B}, pathRest...)
+// // 		pathA := MerklePath{
+// // 			BlockHeight: BRC74JSON.BlockHeight,
+// // 			Path:        append([][]PathElement{path0A}, pathRest...),
+// // 		}
+// // 		pathB := MerklePath{
+// // 			BlockHeight: BRC74JSON.BlockHeight,
+// // 			Path:        append([][]PathElement{path0B}, pathRest...),
+// // 		}
+// // 		root, _ := pathA.ComputeRoot(BRC74TXID2)
+// // 		assert.Equal(t, BRC74Root, root)
+// // 		assert.Panics(t, func() { pathA.ComputeRoot(BRC74TXID3) })
+// // 		assert.Panics(t, func() { pathB.ComputeRoot(BRC74TXID2) })
+// // 		assert.Equal(t, BRC74Root, pathB.ComputeRoot(BRC74TXID3))
+// // 		pathA.Combine(pathB)
+// // 		assert.Equal(t, BRC74Root, pathA.ComputeRoot(BRC74TXID2))
+// // 		assert.Equal(t, BRC74Root, pathA.ComputeRoot(BRC74TXID3))
+// // 	})
+// // }
