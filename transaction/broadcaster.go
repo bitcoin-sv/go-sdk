4e3cc777db219e3bb7290533a69695ac9782250f
David Case <david.case@shruggr.cloud>
2024-04-09 14:20:30 -0400

    update broadcaster api

--- transaction/broadcaster.go
+++ transaction/broadcaster.go
@@ -8,21 +8,23 @@ var (
 )
 
 type BroadcastSuccess struct {
-	Status  BroadcastStatus `json:"status"`
-	Txid    string          `json:"txid"`
-	Message string          `json:"message"`
+	Txid    string `json:"txid"`
+	Message string `json:"message"`
 }
 
 type BroadcastFailure struct {
-	Status      BroadcastStatus `json:"status"`
-	Code        string          `json:"code"`
-	Description string          `json:"description"`
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+func (e *BroadcastFailure) Error() string {
+	return e.Description
 }
 
 type Broadcaster interface {
-	Broadcast(tx *Tx) (interface{}, error)
+	Broadcast(tx *Tx) (*BroadcastSuccess, *BroadcastFailure)
 }
 
-func (t *Tx) Broadcast(b Broadcaster) (interface{}, error) {
+func (t *Tx) Broadcast(b Broadcaster) (*BroadcastSuccess, *BroadcastFailure) {
 	return b.Broadcast(t)
 }
