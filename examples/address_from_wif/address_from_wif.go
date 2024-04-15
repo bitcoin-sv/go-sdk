bc70146266feff00e93e57ca1df10e42fd304865
Luke Rohenaz <rohenaz@gmail.com>
2024-04-15 10:10:45 -0400

    remove error handling in example

--- examples/address_from_wif/address_from_wif.go
+++ examples/address_from_wif/address_from_wif.go
@@ -7,16 +7,10 @@ import (
 
 func main() {
 
-	wif, err := wif.DecodeWIF("Kxfd8ABTYZHBH3y1jToJ2AUJTMVbsNaqQsrkpo9gnnc1JXfBH8mn")
-	if err != nil {
-		panic(err)
-	}
+	wif, _ := wif.DecodeWIF("Kxfd8ABTYZHBH3y1jToJ2AUJTMVbsNaqQsrkpo9gnnc1JXfBH8mn")
 
 	// Print the private key
-	address, err := bscript.NewAddressFromPublicKey(wif.PrivKey.PubKey(), true)
-	if err != nil {
-		panic(err)
-	}
+	address, _ := bscript.NewAddressFromPublicKey(wif.PrivKey.PubKey(), true)
 
 	// Print the address, and the pubkey hash
 	println(address.AddressString, address.PublicKeyHash)
