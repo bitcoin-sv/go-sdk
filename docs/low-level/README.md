4628f829cd6dc1f315496d8bc28434788e53c853
Jake Jones <jake.jones@bsvblockchain.org>
2024-05-16 16:04:08 -0600

    [docs] added type 42 to low-level

--- docs/low-level/README.md
+++ docs/low-level/README.md
@@ -1,4 +1,3 @@
 # Low-Level Constructs
 
 These documents cover the lower level functionalities of the BSV library. These are the building blocks for higher level functionality and are not intended for general use.
-
---
+++ docs/low-level/type_42/type_42.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	ec "github.com/bitcoin-sv/go-sdk/primitives/ec"
+	hash "github.com/bitcoin-sv/go-sdk/primitives/hash"
+)
+
+func main() {
+	// Alice and Bob generate their private keys
+	alicePrivKey, err := ec.NewPrivateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	alicePubKey := alicePrivKey.PubKey()
+
+	bobPrivKey, err := ec.NewPrivateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	bobPubKey := bobPrivKey.PubKey()
+
+	// Both parties agree on an invoice number to use
+	invoiceNumber := "2-simple signing protocol-1"
+
+	// Alice derives a child private key for signing
+	aliceSigningChild, err := alicePrivKey.DeriveChild(bobPubKey, invoiceNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Alice signs a message for Bob
+	message := hash.Sha256([]byte("Hi Bob"))
+	signature, err := aliceSigningChild.Sign(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Bob derives Alice's correct signing public key from her master public key
+	aliceSigningPub, err := alicePubKey.DeriveChild(bobPrivKey, invoiceNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Now, Bob can privately verify Alice's signature
+	verified := aliceSigningPub.Verify(message, signature)
+	fmt.Println("Verified:", verified)
+	// Output should be true if everything is correct
+}
