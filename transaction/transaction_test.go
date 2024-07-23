eedf217fba36538c9ea6ca9b4aa0d27dd49cf6a3
Satchmo <rohenaz@gmail.com>
2024-07-23 10:09:26 -0400

    linting transaction package wip

--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/bitcoin-sv/go-sdk/script"
 	"github.com/bitcoin-sv/go-sdk/transaction"
 	"github.com/bitcoin-sv/go-sdk/transaction/template/p2pkh"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 const BRC62Hex = "0100beef01fe636d0c0007021400fe507c0c7aa754cef1f7889d5fd395cf1f785dd7de98eed895dbedfe4e5bc70d1502ac4e164f5bc16746bb0868404292ac8318bbac3800e4aad13a014da427adce3e010b00bc4ff395efd11719b277694cface5aa50d085a0bb81f613f70313acd28cf4557010400574b2d9142b8d28b61d88e3b2c3f44d858411356b49a28a4643b6d1a6a092a5201030051a05fc84d531b5d250c23f4f886f6812f9fe3f402d61607f977b4ecd2701c19010000fd781529d58fc2523cf396a7f25440b409857e7e221766c57214b1d38c7b481f01010062f542f45ea3660f86c013ced80534cb5fd4c19d66c56e7e8c5d4bf2d40acc5e010100b121e91836fd7cd5102b654e9f72f3cf6fdbfd0b161c53a9c54b12c841126331020100000001cd4e4cac3c7b56920d1e7655e7e260d31f29d9a388d04910f1bbd72304a79029010000006b483045022100e75279a205a547c445719420aa3138bf14743e3f42618e5f86a19bde14bb95f7022064777d34776b05d816daf1699493fcdf2ef5a5ab1ad710d9c97bfb5b8f7cef3641210263e2dee22b1ddc5e11f6fab8bcd2378bdd19580d640501ea956ec0e786f93e76ffffffff013e660000000000001976a9146bfd5c7fbe21529d45803dbcf0c87dd3c71efbc288ac0000000001000100000001ac4e164f5bc16746bb0868404292ac8318bbac3800e4aad13a014da427adce3e000000006a47304402203a61a2e931612b4bda08d541cfb980885173b8dcf64a3471238ae7abcd368d6402204cbf24f04b9aa2256d8901f0ed97866603d2be8324c2bfb7a37bf8fc90edd5b441210263e2dee22b1ddc5e11f6fab8bcd2378bdd19580d640501ea956ec0e786f93e76ffffffff013c660000000000001976a9146bfd5c7fbe21529d45803dbcf0c87dd3c71efbc288ac0000000000"
@@ -32,28 +32,28 @@ func TestNewTransaction(t *testing.T) {
 
 		// Create a new P2PKH unlocker from the private key
 		unlocker, err := p2pkh.Unlock(priv, nil)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		// Add an input
 		tx.AddInputFromTx(sourceTransaction, 0, unlocker)
 
 		// Create a new P2PKH locking script from the address
 		lock, err := p2pkh.Lock(address)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		// Add the outputs
 		tx.AddOutput(&transaction.TransactionOutput{
 			LockingScript: lock,
 			Satoshis:      1,
 		})
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		// Sign the transaction
 		if err := tx.Sign(); err != nil {
-			assert.NoError(t, err)
+			require.NoError(t, err)
 		}
 
 		_, err = tx.BEEF()
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	})
 }
 
@@ -61,20 +61,20 @@ func TestBEEF(t *testing.T) {
 	t.Parallel()
 	t.Run("deserialize and serialize", func(t *testing.T) {
 		tx, err := transaction.NewTransactionFromBEEFHex(BRC62Hex)
-		assert.NoError(t, err)
-		assert.Equal(t, tx.Inputs[0].SourceTransaction.MerklePath.BlockHeight, uint32(814435))
+		require.NoError(t, err)
+		require.Equal(t, tx.Inputs[0].SourceTransaction.MerklePath.BlockHeight, uint32(814435))
 		beef, err := tx.BEEFHex()
-		assert.NoError(t, err)
-		assert.Equal(t, beef, BRC62Hex)
+		require.NoError(t, err)
+		require.Equal(t, beef, BRC62Hex)
 	})
 }
 
 func TestEF(t *testing.T) {
 	t.Run("Serialization and deserialization", func(t *testing.T) {
 		tx, err := transaction.NewTransactionFromBEEFHex(BRC62Hex)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		ef, err := tx.EFHex()
-		assert.NoError(t, err)
-		assert.Equal(t, ef, "010000000000000000ef01ac4e164f5bc16746bb0868404292ac8318bbac3800e4aad13a014da427adce3e000000006a47304402203a61a2e931612b4bda08d541cfb980885173b8dcf64a3471238ae7abcd368d6402204cbf24f04b9aa2256d8901f0ed97866603d2be8324c2bfb7a37bf8fc90edd5b441210263e2dee22b1ddc5e11f6fab8bcd2378bdd19580d640501ea956ec0e786f93e76ffffffff3e660000000000001976a9146bfd5c7fbe21529d45803dbcf0c87dd3c71efbc288ac013c660000000000001976a9146bfd5c7fbe21529d45803dbcf0c87dd3c71efbc288ac00000000")
+		require.NoError(t, err)
+		require.Equal(t, ef, "010000000000000000ef01ac4e164f5bc16746bb0868404292ac8318bbac3800e4aad13a014da427adce3e000000006a47304402203a61a2e931612b4bda08d541cfb980885173b8dcf64a3471238ae7abcd368d6402204cbf24f04b9aa2256d8901f0ed97866603d2be8324c2bfb7a37bf8fc90edd5b441210263e2dee22b1ddc5e11f6fab8bcd2378bdd19580d640501ea956ec0e786f93e76ffffffff3e660000000000001976a9146bfd5c7fbe21529d45803dbcf0c87dd3c71efbc288ac013c660000000000001976a9146bfd5c7fbe21529d45803dbcf0c87dd3c71efbc288ac00000000")
 	})
 }
--- transaction/txoutput_test.go
+++ transaction/txoutput_test.go
@@ -8,7 +8,7 @@ import (
 	script "github.com/bitcoin-sv/go-sdk/script"
 	"github.com/bitcoin-sv/go-sdk/transaction"
 	"github.com/bitcoin-sv/go-sdk/transaction/template/p2pkh"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewP2PKHOutputFromPubKeyHashStr(t *testing.T) {
@@ -18,10 +18,10 @@ func TestNewP2PKHOutputFromPubKeyHashStr(t *testing.T) {
 		// This is the PKH for address mtdruWYVEV1wz5yL7GvpBj4MgifCB7yhPd
 		pkh, _ := hex.DecodeString("8fe80c75c9560e8b56ed64ea3c26e18d2c52211b")
 		add, err := script.NewAddressFromPublicKeyHash(pkh, true)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		s, err := p2pkh.Lock(add)
-		assert.NoError(t, err)
-		assert.Equal(t,
+		require.NoError(t, err)
+		require.Equal(t,
 			"76a9148fe80c75c9560e8b56ed64ea3c26e18d2c52211b88ac",
 			hex.EncodeToString(*s),
 		)
@@ -34,15 +34,15 @@ func TestNewHashPuzzleOutput(t *testing.T) {
 	t.Run("invalid public key", func(t *testing.T) {
 		tx := transaction.NewTransaction()
 		err := tx.AddHashPuzzleOutput("", "0", uint64(5000))
-		assert.Error(t, err)
+		require.Error(t, err)
 	})
 
 	t.Run("missing secret and public key", func(t *testing.T) {
 		tx := transaction.NewTransaction()
 		err := tx.AddHashPuzzleOutput("", "", uint64(5000))
 
-		assert.NoError(t, err)
-		assert.Equal(t,
+		require.NoError(t, err)
+		require.Equal(t,
 			"a914b472a266d0bd89c13706a4132ccfb16f7c3b9fcb8876a90088ac",
 			tx.Outputs[0].LockingScriptHex(),
 		)
@@ -50,14 +50,14 @@ func TestNewHashPuzzleOutput(t *testing.T) {
 
 	t.Run("valid puzzle output", func(t *testing.T) {
 		addr, err := script.NewAddressFromString("myFhJggmsaA2S8Qe6ZQDEcVCwC4wLkvC4e")
-		assert.NoError(t, err)
-		assert.NotNil(t, addr)
+		require.NoError(t, err)
+		require.NotNil(t, addr)
 
 		tx := transaction.NewTransaction()
 		err = tx.AddHashPuzzleOutput("secret1", addr.PublicKeyHash.String(), uint64(5000))
 
-		assert.NoError(t, err)
-		assert.Equal(t,
+		require.NoError(t, err)
+		require.Equal(t,
 			"a914d3f9e3d971764be5838307b175ee4e08ba427b908876a914c28f832c3d539933e0c719297340b34eee0f4c3488ac",
 			tx.Outputs[0].LockingScriptHex(),
 		)
@@ -74,13 +74,13 @@ func TestNewOpReturnOutput(t *testing.T) {
 
 	tx := transaction.NewTransaction()
 	err := tx.AddOpReturnOutput(dataBytes)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	script := tx.Outputs[0].LockingScriptHex()
 	dataLength := transaction.VarInt(uint64(len(dataBytes))).Bytes()
 
-	assert.Equal(t, "006a4d2201"+hex.EncodeToString(dataBytes), script)
-	assert.Equal(t, "fd2201", fmt.Sprintf("%x", dataLength))
+	require.Equal(t, "006a4d2201"+hex.EncodeToString(dataBytes), script)
+	require.Equal(t, "fd2201", fmt.Sprintf("%x", dataLength))
 }
 
 func TestNewOpReturnPartsOutput(t *testing.T) {
@@ -89,9 +89,9 @@ func TestNewOpReturnPartsOutput(t *testing.T) {
 	dataBytes := [][]byte{[]byte("hi"), []byte("how"), []byte("are"), []byte("you")}
 	tx := transaction.NewTransaction()
 	err := tx.AddOpReturnPartsOutput(dataBytes)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
-	assert.Equal(t, "006a02686903686f770361726503796f75", tx.Outputs[0].LockingScriptHex())
+	require.Equal(t, "006a02686903686f770361726503796f75", tx.Outputs[0].LockingScriptHex())
 }
 
 func TestTx_TotalOutputSatoshis(t *testing.T) {
@@ -99,14 +99,14 @@ func TestTx_TotalOutputSatoshis(t *testing.T) {
 
 	t.Run("greater than zero", func(t *testing.T) {
 		tx, err := transaction.NewTransactionFromHex("020000000180f1ada3ad8e861441d9ceab40b68ed98f13695b185cc516226a46697cc01f80010000006b483045022100fa3a0f8fa9fbf09c372b7a318fa6175d022c1d782f7b8bc5949a7c8f59ce3f35022005e0e84c26f26d892b484ff738d803a57626679389c8b302939460dab29a5308412103e46b62eea5db5898fb65f7dc840e8a1dbd8f08a19781a23f1f55914f9bedcd49feffffff02dec537b2000000001976a914ba11bcc46ecf8d88e0828ddbe87997bf759ca85988ac00943577000000001976a91418392a59fc1f76ad6a3c7ffcea20cfcb17bda9eb88ac6e000000")
-		assert.NoError(t, err)
-		assert.NotNil(t, tx)
-		assert.Equal(t, uint64((29.89999582+20.00)*1e8), tx.TotalOutputSatoshis())
+		require.NoError(t, err)
+		require.NotNil(t, tx)
+		require.Equal(t, uint64((29.89999582+20.00)*1e8), tx.TotalOutputSatoshis())
 	})
 
 	t.Run("zero Outputs", func(t *testing.T) {
 		tx := transaction.NewTransaction()
-		assert.NotNil(t, tx)
-		assert.Equal(t, uint64(0), tx.TotalOutputSatoshis())
+		require.NotNil(t, tx)
+		require.Equal(t, uint64(0), tx.TotalOutputSatoshis())
 	})
 }
