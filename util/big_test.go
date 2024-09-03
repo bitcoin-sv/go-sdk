28416318209a5fa29955fed3cc0a85d48ce58e0e
Satchmo <rohenaz@gmail.com>
2024-09-03 13:42:34 -0400

    add test for util/big.go

---
+++ util/big_test.go
@@ -0,0 +1,19 @@
+package util_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/bitcoin-sv/go-sdk/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBigUmod(t *testing.T) {
+	require.Equal(t, int64(2), util.Umod(big.NewInt(-178), big.NewInt(10)).Int64())
+}
+
+func TestBigRandomInt(t *testing.T) {
+	b := util.NewRandomBigInt(32)
+	require.NotNil(t, b)
+	require.Equal(t, 32, len(b.Bytes()))
+}
