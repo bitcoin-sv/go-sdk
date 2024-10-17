6ed7ab7182fdf135bf4ea5103180b30e2c177bae
wregulski <wojciechregulski@outlook.com>
2024-10-17 09:48:34 +0200

    fix: proper loop on totalKeyShares function

--- primitives/ec/privatekey.go
+++ primitives/ec/privatekey.go
@@ -253,7 +253,7 @@ func (p *PrivateKey) ToKeyShares(threshold int, totalShares int) (keyShares *key
 	}
 
 	points := make([]*keyshares.PointInFiniteField, 0)
-	for range totalShares {
+	for i := 0; i < totalShares; i++ {
 		pk, err := NewPrivateKey()
 		if err != nil {
 			return nil, err
