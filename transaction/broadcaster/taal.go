da28253785cf4dc4492106ec8da9eeb48b988395
David Case <david@fyxgaming.com>
2024-10-22 10:13:05 -0400

    add TAAL and WOC broadcasters

---
+++ transaction/broadcaster/taal.go
@@ -0,0 +1,67 @@
+package broadcaster
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/bitcoin-sv/go-sdk/transaction"
+)
+
+type TAALResponse struct {
+	Txid   string `json:"txid"`
+	Status uint32 `json:"status"`
+	Err    string `json:"error"`
+}
+
+type TAALBroadcast struct {
+	ApiKey string
+}
+
+func (b *TAALBroadcast) Broadcast(ctx context.Context, t *transaction.Transaction) (*transaction.BroadcastSuccess, *transaction.BroadcastFailure) {
+	buf := bytes.NewBuffer(t.Bytes())
+	url := "https://api.taal.com/api/v1/broadcast"
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"POST",
+		url,
+		buf,
+	)
+	if err != nil {
+		return nil, &transaction.BroadcastFailure{
+			Code:        "500",
+			Description: err.Error(),
+		}
+	}
+	req.Header.Set("Content-Type", "application/octet-stream")
+	if b.ApiKey != "" {
+		req.Header.Set("Authorization", b.ApiKey)
+	}
+	if resp, err := http.DefaultClient.Do(req); err != nil {
+		return nil, &transaction.BroadcastFailure{
+			Code:        "500",
+			Description: err.Error(),
+		}
+	} else {
+		defer resp.Body.Close()
+		var taalResp TAALResponse
+		if err := json.NewDecoder(resp.Body).Decode(&taalResp); err != nil {
+			return nil, &transaction.BroadcastFailure{
+				Code:        fmt.Sprintf("%d", resp.StatusCode),
+				Description: "unknown error",
+			}
+		} else if resp.StatusCode != 200 && !strings.Contains(taalResp.Err, "txn-already-known") {
+			return nil, &transaction.BroadcastFailure{
+				Code:        fmt.Sprintf("%d", resp.StatusCode),
+				Description: string(taalResp.Err),
+			}
+		} else {
+			return &transaction.BroadcastSuccess{
+				Txid: t.TxID().String(),
+			}, nil
+		}
+	}
+}
